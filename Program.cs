d0e679f4642ca31744fc33d2da1d0d7e8c042647
Piotr <piotrk2006@pm.me>
2022-10-16 20:39:48 +0200

    test

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Nowak1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sprawdź czy liczba jest trzycyfrowa i podzielna przez 17
+            int x = int.Parse(Console.ReadLine());
+            int y = 0;
+
+            while (x>0)
+            {
+                y = x % 10;
+                x /= 10;
+            }
+
+            System.Console.WriteLine(y);
+        }
+    }
+}
